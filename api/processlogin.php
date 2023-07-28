866ea19d765596957d63534e0058075e1ccbabea
daniel.albergucci <daniel.albergucci@epitech.eu>
2023-07-28 11:35:09 +0200

    [~] remaster project [~]

---
+++ api/processlogin.php
@@ -0,0 +1,37 @@
+<?php
+require_once "../config/config.php";
+$error = array();
+$res = array();
+
+if (isset($_POST["newusername"]) && isset($_POST["newpassword"])) {
+	$username = $_POST["newusername"];
+
+	$password = $_POST["newpassword"];
+
+	$stmt = $db->prepare("SELECT `IDUsers`, `username`, `password` FROM `users` WHERE `username` = :uname");
+
+	$stmt->execute(array(':uname' => $_POST['newusername']));
+	$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	if (count($row) > 0) {
+		if (!password_verify($_POST['newpassword'], $row[0]['password'])) {
+			$error[] = "Password is not valid";
+			$resp['msg'] = $error;
+			$resp['status'] = false;
+			echo json_encode($resp);
+			exit;
+		}
+		session_start();
+		$_SESSION['user_id'] = $row[0]['IDUsers'];
+		$resp['redirect'] = "collection.html";
+		$resp['status'] = true;
+		echo json_encode($resp);
+		exit;
+	} else {
+		$error[] = "User does not match";
+		$resp['msg'] = $error;
+		$resp['status'] = false;
+		echo json_encode($resp);
+		exit;
+	}
+}
+?>
---
+++ js/navigation.js
@@ -0,0 +1,15 @@
+function GoActualite() {
+    window.location = '/pages/actualites.html';
+}
+
+function GoCollection() {
+    window.location = '/pages/collection.html';
+}
+
+function GoLogin() {
+    window.location = '/pages/authentification/login.html';
+}
+
+function GoRegister() {
+    window.location = '/pages/authentification/register.html';
+}
